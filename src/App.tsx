cb75c2b1a161459617345eeb91de176e899d2ba8
branton <937935598@qq.com>
2023-09-14 15:56:35 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+const App = (): ReactElement => {
+  return (
+    <div className="App">
+      这是 craco v1.0.2
+    </div>
+  );
+}
+
+export default App;
