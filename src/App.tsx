9bc0a90bde6e83ffea176fb944982108d2e01d95
branton <937935598@qq.com>
2023-09-14 16:16:55 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from 'react';
 const App = (): ReactElement => {
   return (
     <div className="App">
-      这是 craco v1.0.2
+      这是 craco v1.0.1
     </div>
   );
 }
